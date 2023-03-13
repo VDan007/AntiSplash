77729bf19e9b2c2092a477e558ab7bb9135a889a
DanielVarjaskéri <varjaskerid@gmail.com>
2023-03-13 10:58:18 +0100

    shoppingCart icon works

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
+import { Context } from "../Context";
 
 
 
 
 
 function Header() {
-    
-
+    const {cartItems} = useContext(Context);
+    console.log(cartItems);
     return (
         <header>
             <Link to="/">
             <h2>Pic Some</h2>
             </Link>
+
             <Link to="/cart">
-                Cart
+                <img src= {cartItems.length == 0 ? "/shoppingCart.svg" : "/shoppingCartFilled.svg"} alt="" />
             </Link>
             
         </header>
