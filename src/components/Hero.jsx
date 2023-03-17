9809071727c523d22699a1daa32dd53eee3cda02
DanielVarjaskéri <varjaskerid@gmail.com>
2023-03-17 21:58:50 +0100

    hero worked

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,13 @@ function Hero(){
                 <h1>Antisplash</h1>
                 <p>The internet's source for visuals.</p>
                 <p>Powered by creators everywhere.</p>
-                <input type="text" />
+                <span className="inputSpan">
+                    <img className="inputIcon" src="/search-line.svg" alt="" />
+                    <input type="text"
+                        placeholder="Search high-resolution images" 
+
+                    />
+                </span>
             </div>
         </div>
     );
