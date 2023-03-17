c24a38255c7970c57f5326de99b391e072675956
DanielVarjaskéri <varjaskerid@gmail.com>
2023-03-17 13:50:14 +0100

    userSignIn works

--- src/components/auth/AuthDetails.jsx
+++ src/components/auth/AuthDetails.jsx
@@ -1,4 +1,4 @@
-import { onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import React, { useEffect, useContext, useState} from "react";
 import { Context } from "../../Context";
 
@@ -7,18 +7,30 @@ function AuthDetails(){
     const [ authUser, setAuthUser] = useState(null);
 
     useEffect(()=>{
-        onAuthStateChanged(auth,(user)=>{
+        const listen = onAuthStateChanged(auth,(user)=>{
             if(user){
                 setAuthUser(user)
             }else{
                 setAuthUser(null);
             }
         });
+        return () => {
+            listen();
+        }
     },[]);
+
+        const userSignOut = () =>{
+            signOut(auth).then(()=>{
+                console.log('user signed out');
+            }).catch(error=>console.log(error));
+        }
 
     return(
         <div>{authUser ? 
-            <p>Signed In</p> 
+            <>
+            <button onClick={userSignOut}>Sign Out</button>
+            <p>{`Signed In as ${authUser.email}`}</p> 
+            </>
             :
             <p>Signed Out</p>}
         </div>
