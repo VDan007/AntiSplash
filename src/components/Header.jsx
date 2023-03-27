65a3fab0673b644bbf6005340f1a7b87d37e26d0
DanielVarjaskéri <varjaskerid@gmail.com>
2023-03-27 10:32:39 +0200

    header shaping up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,14 +13,24 @@ function Header() {
     
     return (
         <header className="header">
-            <Link to="/">
-            <img src="/unsplash-fill.svg" className="logoImg" alt="" />
-            </Link>
-            <div className="headerRightDiv">
-                <AuthDetails/>
-                <Link to="/cart">
-                    <img src= {cartItems.length == 0 ? "/shoppingCart.svg" : "/shoppingCartFilled.svg"} alt="" />
+            <div className="headerUpper">
+                <Link to="/">
+                <img src="/unsplash-fill.svg" className="logoImg" alt="" />
                 </Link>
+                <div className="headerRightDiv">
+                    <AuthDetails/>
+                    <Link to="/cart">
+                        <img src= {cartItems.length == 0 ? "/shoppingCart.svg" : "/shoppingCartFilled.svg"} alt="" />
+                    </Link>
+                </div>
+            </div>
+            <div className="headerCatergorySearchBar">
+                <div className="headerCatergorySearchBarFix">
+                    <p>Editorial</p>
+                    <p>Following</p>
+                </div>
+                
+                <div></div>
             </div>
         </header>
     )
