bbbe69fc0bfac4d4922476cf819ad2c336ae6c3e
DanielVarjaskéri <varjaskerid@gmail.com>
2023-03-27 12:36:24 +0200

    header bugs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,34 @@ function Header() {
                     <p>Following</p>
                 </div>
                 
-                <div></div>
+                <div className='headerCatergorySearchBarCarusel'>
+                    <div className='arrowDiv aDl'>sf</div>
+                    <div className='arrowDiv aDr'>sdf</div>
+                    <div className="ulContainer">
+                        <ul>
+                            <li>Wallpapers</li>
+                            <li>3D Renders</li>
+                            <li>Travel</li>
+                            <li>Nature</li>
+                            <li>Street Photography</li>
+                            <li>Experimental</li>
+                            <li>Textures & Patterns</li>
+                            <li>Animals</li>
+                            <li>Architectures & Interiors</li>
+                            <li>Fashion & Beauty</li>
+                            <li>Film</li>
+                            <li>Food & Drink</li>
+                            <li>People</li>
+                            <li>Spirituality</li>
+                            <li>Business & Work</li>
+                            <li>Athletics</li>
+                            <li>Health & Wellness</li>
+                            <li>Current Events</li>
+                            <li>Arts & Culture</li>
+                        </ul>
+
+                    </div>
+                </div>
             </div>
         </header>
     )
